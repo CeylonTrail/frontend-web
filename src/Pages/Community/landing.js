1b207f30fc7385fa092a54e4a7f89dffe5ea0c3e
IsuriCS <isurics2@gmail.com>
2024-07-30 01:07:08 +0530

    Community Posts

--- src/Pages/Community/landing.js
+++ src/Pages/Community/landing.js
@@ -1,13 +1,27 @@
 import Header from "../../components/header";
 import FilterBar from "../../components/sidebarFilters";
+import CommunityPost from "../../components/communityPost";
+import {SimpleInput} from "../../components/inputFields";
+import Profile from "../../assets/img/Profile.svg";
+import { useState } from "react";
 
-export default () => { 
-    
+export default () => {
+    const [showPopup, setShowPopup] = useState(false);
+
     return (
         <>
             <Header />
-            <FilterBar />
+            <div className="flex flex-row justify-around">
+                <FilterBar />
+                <div className="flex flex-col items-center mt-24 w-full max-w-screen-md mx-auto">
+                    <span className="bg-white p-4 flex flex-row gap-1 px-8 ml-4 mr-4 w-[704px] shadow-lg rounded-md mb-4" onClick={setShowPopup('true')}>
+                        {console.log(showPopup)}
+                        <img src={Profile} className="w-12" alt="Profile" />
+                        <SimpleInput pholder={"Create a post"} className="w-full ml-4" />
+                    </span>
+                    <CommunityPost />
+                </div>
+            </div>
         </>
-       
-    )
+    );
 };
